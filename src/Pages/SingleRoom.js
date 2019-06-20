fc3575aa645127a0274166e403ee62f130d01679
olumide ogundare <smallboy@gmail.com>
2019-06-20 23:55:17 +0100

    setup Singlerooms  file

---
+++ src/Pages/SingleRoom.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+
+class SingleRoom extends Component {
+  state = {};
+
+  render() {
+    return <div>SingleRoom</div>;
+  }
+}
+export default SingleRoom;
